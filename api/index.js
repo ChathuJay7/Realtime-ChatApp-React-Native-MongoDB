c1cfd0f31b2e3f11b5567e6f0cde0a04ab042b7a
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-07 15:37:06 +0530

    Add project structure and setup backend

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv")
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+dotenv.config();
+// connections
+mongoose
+  .connect(`${process.env.MONGODB_URL}`)
+  .then(() => {
+    console.log("Connected to Mongo Db");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server running on port 8000");
+});
