12b2aac88b4c5325c310126e3bc806c8da22569a
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-08 09:42:19 +0530

    Add useContext to store userId

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { UserType } from "../UserContecx";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  const {userId, setUserId} = useContext(UserType);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+      headerLeft: () => <Text style={{ fontSize: 16, fontWeight: "bold" }}>Swift Chat</Text>,
+      headerRight: () => (
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+          <Ionicons onPress={() => navigation.navigate("Chats")} name="chatbox-ellipses-outline" size={24} color="black" />
+          <MaterialIcons
+            onPress={() => navigation.navigate("Friends")}
+            name="people-outline"
+            size={24}
+            color="black"
+          />
+        </View>
+      ),
+    });
+  }, []);
+
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
