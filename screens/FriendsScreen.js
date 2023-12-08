a23c9f4851a8c478405add26e21cc31c6aa20fb4
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-08 15:48:53 +0530

    Fetch all friend requests of particular user in frontend

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { UserType } from "../UserContecx";
+import axios from "axios";
+import FriendRequest from "../components/FriendRequest";
 
 const FriendsScreen = () => {
+  const { userId, setUserId } = useContext(UserType);
+  const [friendRequests, setFriendRequests] = useState([]);
+
+  useEffect(() => {
+    fetchFriendRequests();
+  }, []);
+
+  const fetchFriendRequests = async () => {
+    try {
+      const response = await axios.get(
+        `http://192.168.8.154:8000/users/friend-request/${userId}`
+      );
+      if (response.status === 200) {
+        const friendRequestsData = response.data.map((friendRequest) => ({
+          _id: friendRequest._id,
+          name: friendRequest.name,
+          email: friendRequest.email,
+          image: friendRequest.image,
+        }));
+
+        setFriendRequests(friendRequestsData);
+        //setFriendRequests(response.data)
+      }
+    } catch (err) {
+      console.log("error message", err);
+    }
+  };
+
+  console.log(friendRequests);
+
   return (
-    <View>
-      <Text>FriendsScreen</Text>
+    <View style={{ padding: 10, marginHorizontal: 12 }}>
+      {friendRequests.length > 0 && <Text>Your Friend Requests!</Text>}
+
+      {friendRequests.map((item, index) => (
+        <FriendRequest
+          key={index}
+          item={item}
+          friendRequests={friendRequests}
+          setFriendRequests={setFriendRequests}
+        />
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default FriendsScreen
+export default FriendsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
