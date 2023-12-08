be47ff4356ab4a721beffa0edb6e5b2ffb57ef4c
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-08 16:10:45 +0530

    Add FriendRequest component to display friend requests

--- components/FriendRequest.js
+++ components/FriendRequest.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { UserType } from "../UserContecx";
 
-const FriendRequest = () => {
+const FriendRequest = ({ item, friendRequests, setFriendRequests }) => {
+  const { userId, setUserId } = useContext(UserType);
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>FriendRequest</Text>
-    </View>
-  )
-}
+    <Pressable
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginVertical: 10,
+      }}
+    >
+      <Image
+        style={{ width: 50, height: 50, borderRadius: 25 }}
+        source={{ uri: item.image }}
+      />
 
-export default FriendRequest
+      <Text
+        style={{ fontSize: 15, fontWeight: "bold", marginLeft: 10, flex: 1 }}
+      >
+        {item?.name} sent you a friend request!!
+      </Text>
 
-const styles = StyleSheet.create({})
+      <TouchableOpacity
+        //onPress={() => acceptRequest(item._id)}
+        style={{ backgroundColor: "#0066b2", padding: 10, borderRadius: 6 }}
+      >
+        <Text style={{ textAlign: "center", color: "white" }}>Accept</Text>
+      </TouchableOpacity>
+    </Pressable>
+  );
+};
+
+export default FriendRequest;
+
+const styles = StyleSheet.create({});
