63ee76aa57b0ebfffd615e41b9f01fc504d00e94
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-08 14:02:36 +0530

    Modified sendFriendRequest function in backend

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -2,41 +2,40 @@ const User = require("../models/User");
 
 //endpoint to access all the users except the user who's is currently logged in!
 const getAllOtherUsers = (req, res) => {
-    const loggedInUserId = req.params.userId;
-  
-    User.find({ _id: { $ne: loggedInUserId } })
-      .then((users) => {
-        res.status(200).json(users);
-      })
-      .catch((err) => {
-        console.log("Error retrieving users", err);
-        res.status(500).json({ message: "Error retrieving users" });
-      });
+  const loggedInUserId = req.params.userId;
+
+  User.find({ _id: { $ne: loggedInUserId } })
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log("Error retrieving users", err);
+      res.status(500).json({ message: "Error retrieving users" });
+    });
 };
 
-
 //endpoint to send a request to a user
-const sendFriendRequest =  async (req, res) => {
-    const { currentUserId, selectedUserId } = req.body;
-  
-    try {
-      //update the recepient's friendRequestsArray!
-      await User.findByIdAndUpdate(selectedUserId, {
-        $push: { freindRequests: currentUserId },
-      });
-  
-      //update the sender's sentFriendRequests array
-      await User.findByIdAndUpdate(currentUserId, {
-        $push: { sentFriendRequests: selectedUserId },
-      });
-  
-      res.sendStatus(200);
-    } catch (error) {
-      res.sendStatus(500);
-    }
-  };
+const sendFriendRequest = async (req, res) => {
+  const { currentUserId, selectedUserId } = req.body;
 
+  try {
+    //update the recepient's friendRequestsArray!
+    await User.findByIdAndUpdate(selectedUserId, {
+      $push: { freindRequests: currentUserId },
+    });
+
+    //update the sender's sentFriendRequests array
+    await User.findByIdAndUpdate(currentUserId, {
+      $push: { sentFriendRequests: selectedUserId },
+    });
+
+    res.status(200).json({ message: "Friend request send" });
+  } catch (error) {
+    res.status(500);
+  }
+};
 
 module.exports = {
-    getAllOtherUsers, sendFriendRequest
+  getAllOtherUsers,
+  sendFriendRequest,
 };
--- api/routing/user-routes.js
+++ api/routing/user-routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAllOtherUsers } = require("../controllers/user-controller");
+const { getAllOtherUsers, sendFriendRequest } = require("../controllers/user-controller");
 
 
 const userRouter = express.Router();
 
 userRouter.get("/:userId", getAllOtherUsers);
-userRouter.post("/friend-request", getAllOtherUsers);
+userRouter.post("/friend-request", sendFriendRequest);
 
 module.exports = userRouter;
