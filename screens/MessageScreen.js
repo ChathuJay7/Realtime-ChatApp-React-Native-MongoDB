6b01ec826030fc3693e066cb6f9cc1ff412f7bdd
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-09 13:44:49 +0530

    Add Text input to message screen and Emoji selector

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,14 +1,105 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Entypo, Feather, Ionicons } from "@expo/vector-icons";
+import { ScrollView } from "react-native";
+import EmojiSelector from "react-native-emoji-selector";
 
 const MessageScreen = () => {
+
+  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleEmojiPress = () => {
+    setShowEmojiSelector(!showEmojiSelector);
+  };
+
   return (
-    <View>
-      <Text>MessageScreen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
+      <ScrollView></ScrollView>
 
-export default MessageScreen
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: 10,
+          paddingVertical: 10,
+          borderTopWidth: 1,
+          borderTopColor: "#dddddd",
+          marginBottom: showEmojiSelector ? 0 : 25,
+        }}
+      >
+        <Entypo
+          onPress={handleEmojiPress}
+          style={{ marginRight: 5 }}
+          name="emoji-happy"
+          size={24}
+          color="gray"
+        />
+        <TextInput
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+          style={{
+            flex: 1,
+            height: 40,
+            borderWidth: 1,
+            borderColor: "#dddddd",
+            borderRadius: 20,
+            paddingHorizontal: 10,
+          }}
+          placeholder="Type Your message..."
+        />
 
-const styles = StyleSheet.create({})
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 7,
+            marginHorizontal: 8,
+          }}
+        >
+          <Entypo
+            //onPress={pickImage}
+            name="camera"
+            size={24}
+            color="gray"
+          />
+
+          <Feather name="mic" size={24} color="gray" />
+        </View>
+
+        <Pressable
+          //onPress={() => handleSend("text")}
+          style={{
+            backgroundColor: "#007bff",
+            paddingVertical: 8,
+            paddingHorizontal: 12,
+            borderRadius: 20,
+          }}
+        >
+          <Ionicons name="send" size={24} color="white" />
+        </Pressable>
+      </View>
+
+      {showEmojiSelector && (
+        <EmojiSelector
+          onEmojiSelected={(emoji) => {
+            setMessage((prevMessage) => prevMessage + emoji);
+          }}
+          style={{ height: 250 }}
+        />
+      )}
+
+    </KeyboardAvoidingView>
+  );
+};
+
+export default MessageScreen;
+
+const styles = StyleSheet.create({});
