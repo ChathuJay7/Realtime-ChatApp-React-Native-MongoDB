90b8fc1186303cc071826a89854295e266048164
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-10 08:59:36 +0530

    Add the header part to message screen

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,4 +1,5 @@
 import {
+  Image,
   KeyboardAvoidingView,
   Pressable,
   StyleSheet,
@@ -6,21 +7,22 @@ import {
   TextInput,
   View,
 } from "react-native";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useLayoutEffect, useState } from "react";
 import { Entypo, Feather, Ionicons } from "@expo/vector-icons";
 import { ScrollView } from "react-native";
 import EmojiSelector from "react-native-emoji-selector";
 import { UserType } from "../UserContecx";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const MessageScreen = () => {
-
   const [showEmojiSelector, setShowEmojiSelector] = useState(false);
   const [message, setMessage] = useState("");
   const [selectedImage, setSelectedImage] = useState("");
-  
+  const [recepientData, setRecepientData] = useState();
+
   const { userId, setUserId } = useContext(UserType);
 
+  const navigation = useNavigation();
   const route = useRoute();
   const { recepientId } = route.params;
 
@@ -49,13 +51,16 @@ const MessageScreen = () => {
 
       console.log("Sending request", formData);
 
-      const response = await fetch("http://192.168.8.154:8000/message/send-message", {
-        method: "POST",
-        body: formData,
-        headers: {
-          "Content-Type": "multipart/form-data",
+      const response = await fetch(
+        "http://192.168.8.154:8000/message/send-message",
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-      });
+      );
 
       if (response.ok) {
         setMessage("");
@@ -67,6 +72,80 @@ const MessageScreen = () => {
       console.log("error in sending the message", error);
     }
   };
+
+
+  useEffect(() => {
+    fetchRecepientData();
+  }, []);
+
+  const fetchRecepientData = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.8.154:8000/users/recepient-details/${recepientId}`
+      );
+
+      const data = await response.json();
+      setRecepientData(data);
+    } catch (error) {
+      console.log("error retrieving details", error);
+    }
+  };
+
+  console.log(recepientData)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+      headerLeft: () => (
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+          <Ionicons
+            onPress={() => navigation.goBack()}
+            name="arrow-back"
+            size={24}
+            color="black"
+          />
+
+          {/* {selectedMessages.length > 0 ? (
+            <View>
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>
+                {selectedMessages.length}
+              </Text>
+            </View>
+          ) : ( */}
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Image
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 15,
+                  resizeMode: "cover",
+                }}
+                source={{ uri: recepientData?.image }}
+              />
+
+              <Text style={{ marginLeft: 5, fontSize: 15, fontWeight: "bold" }}>
+                {recepientData?.name}
+              </Text>
+            </View>
+          {/* )} */}
+        </View>
+      ),
+      // headerRight: () =>
+      //   selectedMessages.length > 0 ? (
+      //     <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+      //       <Ionicons name="md-arrow-redo-sharp" size={24} color="black" />
+      //       <Ionicons name="md-arrow-undo" size={24} color="black" />
+      //       <FontAwesome name="star" size={24} color="black" />
+      //       <MaterialIcons
+      //         onPress={() => deleteMessages(selectedMessages)}
+      //         name="delete"
+      //         size={24}
+      //         color="black"
+      //       />
+      //     </View>
+      //   ) : null,
+    });
+  }, [recepientData]);
 
   return (
     <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
@@ -143,7 +222,6 @@ const MessageScreen = () => {
           style={{ height: 250 }}
         />
       )}
-
     </KeyboardAvoidingView>
   );
 };
