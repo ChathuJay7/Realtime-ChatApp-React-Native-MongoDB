2d6d230224350f779472f832afc15732e075ab85
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-10 15:18:18 +0530

    Add last message function in frontend

--- components/UserChat.js
+++ components/UserChat.js
@@ -1,10 +1,57 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { UserType } from "../UserContecx";
 
 const UserChat = ({ item }) => {
+
+  const [messages, setMessages] = useState([]);
+  
+  const {userId, setUserId} = useContext(UserType);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    fetchMessages();
+  }, [messages])
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.8.154:8000/message/${userId}/${item._id}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessages(data);
+      } else {
+        console.log("error showing messags", response.status.message);
+      }
+    } catch (error) {
+      console.log("error fetching messages", error);
+    }
+  };
+
+  
+
+  const getLastMessage = () => {
+    const userMessages = messages.filter(
+      (message) => message.messageType === "text"
+    );
+
+    const n = userMessages.length;
+
+    return userMessages[n - 1];
+  };
+
+  const lastMessage = getLastMessage();
+
+
+
+  const formatTime = (time) => {
+    const options = { hour: "numeric", minute: "numeric" };
+    return new Date(time).toLocaleString("en-US", options);
+  };
 
   return (
     <TouchableOpacity
@@ -32,20 +79,16 @@ const UserChat = ({ item }) => {
 
       <View style={{ flex: 1 }}>
         <Text style={{ fontSize: 15, fontWeight: "500" }}>{item?.name}</Text>
-        {/* {lastMessage && (
+        {lastMessage && (
           <Text style={{ marginTop: 3, color: "gray", fontWeight: "500" }}>
             {lastMessage?.message}
           </Text>
-        )} */}
-        <Text style={{ marginTop: 3, color: "gray", fontWeight: "500" }}>
-          Last Message
-        </Text>
+        )}
       </View>
 
       <View>
         <Text style={{ fontSize: 11, fontWeight: "400", color: "#585858" }}>
-          {/* {lastMessage && formatTime(lastMessage?.timeStamp)} */}
-          11.19 p.m
+          {lastMessage && formatTime(lastMessage?.timeStamp)}
         </Text>
       </View>
     </TouchableOpacity>
